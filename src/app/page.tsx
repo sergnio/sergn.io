e0e79de3a2fa60b53da6ea498db61a71a663431f
sergnio <sergnioa@gmail.com>
2024-09-19 21:10:35 -0500

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,20 @@
+"use client";
+
+import { useState } from "react";
+
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default () => {
+  const [count, setCount] = useState<number>(0);
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <h2>Count: {count}</h2>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+        <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+          Increment
+        </button>
       </main>
       <footer className={styles.footer}>
         <a
@@ -92,4 +62,4 @@ export default function Home() {
       </footer>
     </div>
   );
-}
+};
