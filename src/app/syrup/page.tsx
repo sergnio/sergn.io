4db7b422ada222d28c5688d9faaa7cfa51c1960f
sergnio <sergnioa@gmail.com>
2024-09-20 14:27:04 -0500

    checkin

--- src/app/syrup/page.tsx
+++ src/app/syrup/page.tsx
@@ -3,12 +3,12 @@ type Syrup = {
   rating: number;
   price: number;
   location: string;
-  image: any;
+  image: string;
 };
 
 const syrups: Syrup[] = [
   {
-    name: "Maple",
+    name: "Sweet Ontario",
     rating: 4.5,
     price: 7.99,
     location: "??",
@@ -16,38 +16,34 @@ const syrups: Syrup[] = [
   },
 ];
 
-export default () => {
-  return (
-    <table>
-      <thead>
+export default () => (
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Rating</th>
+        <th>Price</th>
+        <th>Location</th>
+        <th>Image</th>
+      </tr>
+    </thead>
+    <tbody>
+      {syrups.length === 0 && (
         <tr>
-          <th>Name</th>
-          <th>Rating</th>
-          <th>Price</th>
-          <th>Location</th>
-          <th>Image</th>
+          <td colSpan={4}>No syrups found</td>
         </tr>
-      </thead>
-      <tbody>
-        {syrups.length === 0 && (
-          <tr>
-            <td colSpan={4}>No syrups found</td>
-          </tr>
-        )}
-        {syrups.map(
-          ({ name, rating, price, location, image, image2 }, index) => (
-            <tr key={index}>
-              <td>{name}</td>
-              <td>{rating}</td>
-              <td>{price}</td>
-              <td>{location}</td>
-              <td>
-                <img src={image} alt={`${name} image`} />
-              </td>
-            </tr>
-          ),
-        )}
-      </tbody>
-    </table>
-  );
-};
+      )}
+      {syrups.map(({ name, rating, price, location, image, image2 }, index) => (
+        <tr key={index}>
+          <td>{name}</td>
+          <td>{rating}</td>
+          <td>{price}</td>
+          <td>{location}</td>
+          <td>
+            <img src={image} alt={`${name} image`} />
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
