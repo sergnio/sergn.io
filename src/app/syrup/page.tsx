20cdd7266ae10239b57d6a11a95be4c19fc7ac73
sergnio <sergnioa@gmail.com>
2024-09-25 15:53:22 -0500

    update

--- src/app/syrup/page.tsx
+++ src/app/syrup/page.tsx
@@ -1,6 +1,9 @@
+import styles from "./syrup.module.css";
+
 type Syrup = {
   name: string;
   rating: number;
+  reason: string;
   price: number;
   location: string;
   image: string;
@@ -9,19 +12,33 @@ type Syrup = {
 const syrups: Syrup[] = [
   {
     name: "Sweet Ontario",
-    rating: 4.5,
+    rating: 5,
+    reason: `I didn't know the incredible wonders of pure maple syrup before trying this. It has the perfect maple syrup flavor without the 
+    disgusting taste of sweetners. It's maple syrup in its purest form.`,
     price: 7.99,
-    location: "??",
-    image: "syrup/sweetOntario.png",
+    location: "Actual Canada",
+    image: "syrup/sweet_ontario_canada.jpg",
+  },
+  {
+    name: "Hidden Springs",
+    rating: 4,
+    reason: `A bit darker, and a bit too sweet. It's drastically sweet compared to Sweet Ontario, but decent maple syrup. Probably wouldn't recommend over other types`,
+    price: 9.99,
+    location: "Amazon",
+    image: "syrup/hidden_springs_amazon.jpg",
   },
 ];
 
+const sortByRating = (a, b) => b.rating - a.rating;
+
+// todo - covert away from table, use "card" format instead
 export default () => (
   <table>
     <thead>
       <tr>
         <th>Name</th>
         <th>Rating</th>
+        <th>Reason</th>
         <th>Price</th>
         <th>Location</th>
         <th>Image</th>
@@ -33,18 +50,25 @@ export default () => (
           <td colSpan={4}>No syrups found</td>
         </tr>
       )}
-      {syrups.map(({ name, rating, price, location, image }, index) => (
-        <tr key={index}>
-          <td>{name}</td>
-          <td>{rating}</td>
-          <td>{price}</td>
-          <td>{location}</td>
-          <td>
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img src={image} alt={`${name} image`} />
-          </td>
-        </tr>
-      ))}
+      {syrups
+        .sort(sortByRating)
+        .map(({ name, rating, reason, price, location, image }, index) => (
+          <tr key={index}>
+            <td>{name}</td>
+            <td>{rating}</td>
+            <td>{reason}</td>
+            <td>{price}</td>
+            <td>{location}</td>
+            <td>
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src={image}
+                alt={`${name} image`}
+                className={styles.syrupImage}
+              />
+            </td>
+          </tr>
+        ))}
     </tbody>
   </table>
 );
