28eea4429e3355af3c9ff23f91ea6d110061e8b5
sergnio <sergnioa@gmail.com>
2024-12-09 01:20:14 -0600

    commit the hundyp

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -5,8 +5,9 @@ type Route = {
 
 const wings = "/wings";
 const syrup = "/syrup";
+const hundyp = "/hundyp";
 
-const routeList = [wings, syrup];
+const routeList = [wings, syrup, hundyp];
 
 export const routes: Route[] = routeList.map((route) => {
   const trimmedRoute = route.replace("/", "");
